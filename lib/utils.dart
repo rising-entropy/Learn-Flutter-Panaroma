2439e3ab9fafdcbac63e2d550a2c354ce7b35e20
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-07-04 16:17:40 +0530

    app

---
+++ lib/utils.dart
@@ -0,0 +1,22 @@
+List<Map> theData = [
+  {
+    "name": "Milky Way View from La Sila",
+    "fileName": "LaSilaMilkyWay.jpg"
+  },
+  {
+    "name": "Inside of Paranal",
+    "fileName": "ParanalIndoor.jpg"
+  },
+  {
+    "name": "Paranal Observatory",
+    "fileName": "ParanalObservatory.jpg"
+  },
+  {
+    "name": "The Picnic Room",
+    "fileName": "PicnicRoom.jpg"
+  },
+  {
+    "name": "Within the VLT",
+    "fileName": "VLTWithin.jpg"
+  }
+];
